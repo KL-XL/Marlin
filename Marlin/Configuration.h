6105260d06e55d9e3763fd04f08b2258a48ec2a1
KL <lapink2000@hotmail.com>
2020-11-28 21:41:26 +0100

    Update Custom Name in Configuration.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -73,7 +73,7 @@
 // @section info
 
 // Author info of this build printed to the host during boot and M115
-#define STRING_CONFIG_H_AUTHOR "(KL, Ender-3 Tuned)" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "(KL Ender-3 SKR E3 MINI V2)" // Who made the changes.
 //#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 /**
@@ -134,7 +134,7 @@
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "Ender-3 KL Tuned"
+#define CUSTOM_MACHINE_NAME "KL Ender-3 SKR E3 MINI V2"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
